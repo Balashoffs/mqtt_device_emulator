4ff2b879c795f007cd82e999df3d86805eef4d66
potpal <bau@liis.su>
2024-07-12 12:32:10 +0300

    add root mqtt topic

--- src/main/java/com/balashoff/Main.java
+++ src/main/java/com/balashoff/Main.java
@@ -10,18 +10,28 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws MqttException {
         String host = "127.0.0.1";
+        String mqttPath = "bimstand";
         int port = 1883;
         if(args.length > 0){
-            if(args.length == 2){
-                host = args[0];
-                String portArg = args[1];
-                port = Integer.parseInt(portArg);
+            for (int i = 0; i< args.length; i+=2){
+                String value = args[i];
+                switch (value){
+                    case "-ip":
+                        host = args[i+1];
+                        break;
+                    case "-port":
+                        port = Integer.parseInt(args[i+1]);
+                        break;
+                    case "-mqtt":
+                        mqttPath = args[i+1];
+                        break;
+                }
             }
         }
         MqttCustomClient customClient = new MqttCustomClient(host, port);
         ServiceFabric serviceFabric = new ServiceFabric();
         serviceFabric.init("devices.json");
-        List<BaseService> bss  = serviceFabric.create(customClient);
+        List<BaseService> bss  = serviceFabric.create(customClient, mqttPath);
         bss.forEach(baseService -> new Thread(baseService).start());
     }
 }
